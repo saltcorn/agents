b5e5280ca1cf80fe7686f0f0892d504e41eb8359
Tom Nielsen <tanielsen@gmail.com>
2026-02-05 10:35:35 +0000

    move action to own file

---
+++ action.js
@@ -0,0 +1,96 @@
+const FieldRepeat = require("@saltcorn/data/models/fieldrepeat");
+const Table = require("@saltcorn/data/models/table");
+const { get_skills, process_interaction } = require("./common");
+const { applyAsync } = require("@saltcorn/data/utils");
+const WorkflowRun = require("@saltcorn/data/models/workflow_run");
+const { interpolate } = require("@saltcorn/data/utils");
+
+module.exports = {
+  disableInBuilder: true,
+  disableInList: true,
+  disableInWorkflow: true,
+  configFields: async ({ table, mode }) => {
+    const skills = get_skills();
+    const skills_fields = [];
+    for (const skill of skills) {
+      if (skill.configFields) {
+        const fields = await applyAsync(skill.configFields, undefined);
+        for (const field of fields) {
+          if (!field.showIf) field.showIf = {};
+          field.showIf.skill_type = skill.skill_name;
+          skills_fields.push(field);
+        }
+      }
+    }
+    const tables_with_json_field = (await Table.find({})).filter(
+      (table) =>
+        !table.external &&
+        !table.provider_name &&
+        table.fields.some((f) => f.type?.name === "JSON"),
+    );
+    return [
+      ...(table
+        ? [
+            {
+              name: "prompt",
+              label: "Prompt",
+              sublabel:
+                "When triggered from table event or table view button. Use handlebars <code>{{}}</code> to access table fields. Ignored if run in Agent Chat view.",
+              type: "String",
+              required: true,
+              attributes: { options: table.fields.map((f) => f.name) },
+            },
+          ]
+        : []),
+      {
+        name: "sys_prompt",
+        label: "System prompt",
+        sublabel:
+          "Additional information for the system prompt. Use interpolations <code>{{ }}</code> to access triggering row variables or user",
+        type: "String",
+        fieldview: "textarea",
+      },
+      {
+        name: "model",
+        label: "Model",
+        sublabel: "Override default model name",
+        type: "String",
+      },
+      new FieldRepeat({
+        name: "skills",
+        label: "Skills",
+        fields: [
+          {
+            name: "skill_type",
+            label: "Type",
+            type: "String",
+            required: true,
+            attributes: { options: skills.map((s) => s.skill_name) },
+          },
+          ...skills_fields,
+        ],
+      }),
+    ];
+  },
+  run: async ({ configuration, user, row, trigger_id, req, ...rest }) => {
+    const userinput = interpolate(configuration.prompt, row, user);
+    const run = await WorkflowRun.create({
+      status: "Running",
+      started_by: user?.id,
+      trigger_id: trigger_id || undefined,
+      context: {
+        implemented_fcall_ids: [],
+        interactions: [{ role: "user", content: userinput }],
+        funcalls: {},
+      },
+    });
+    return await process_interaction(
+      run,
+      configuration,
+      req,
+      undefined,
+      [],
+      row,
+    );
+  },
+};
