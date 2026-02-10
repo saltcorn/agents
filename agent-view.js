d8af7683480e5ccf32cd3a5d6a22a55f56cc4299
Tom Nielsen <tanielsen@gmail.com>
2026-02-10 11:12:11 +0000

    shift + enter = line break

--- agent-view.js
+++ agent-view.js
@@ -674,7 +674,7 @@ const run = async (
         }
     }
     function submitOnEnter(event) {
-        if (event.which === 13) {
+        if (event.which === 13 && !event.shiftKey) {
             if (!event.repeat) {
                 const newEvent = new Event("submit", {cancelable: true});
                 event.target.form.dispatchEvent(newEvent);
