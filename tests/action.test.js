a9b93559b399248b1e04730289ab2d73e69cbccb
Tom Nielsen <tanielsen@gmail.com>
2026-03-20 18:29:44 +0000

    clarify test

--- tests/action.test.js
+++ tests/action.test.js
@@ -128,7 +128,7 @@ for (const nameconfig of require("./configs")) {
       const result = await action.run({
         row: {
           theprompt:
-            "What is the 16th Fibonacci number (when F1=1 and F2=1)? Consult both the math agent and the oracle and see if they agree",
+            "What is the 16th Fibonacci number (when F1=1 and F2=1)? Consult both the math agent and the oracle and see if they agree. You must call both the OracleAgent and the MathsAgent tools simultaneously",
         },
         configuration,
         user,
