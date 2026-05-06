acd130c9ada6880b2d0a9c48b4aa7b7f404c310b
Tom Nielsen <tanielsen@gmail.com>
2026-05-06 18:28:18 +0100

    tools

--- skills/LongTermMemory.js
+++ skills/LongTermMemory.js
@@ -79,12 +79,6 @@ class LongTermMemory {
     });
     await Field.create({
       table,
-      name: "memory_type",
-      label: "Memory type",
-      type: "String",
-    });
-    await Field.create({
-      table,
       name: "topic",
       label: "Topic",
       type: "String",
@@ -103,7 +97,38 @@ class LongTermMemory {
     return [
       {
         type: "function",
+        function: {
+          name: "store_memory",
+          description: `Store a fact or observation in long-term memory`,
+          parameters: {
+            type: "object",
+            required: ["contents"],
+            properties: {
+              contents: {
+                type: "string",
+                description: "The contents of the fact or observations",
+              },
+            },
+          },
+        },
+        process: async (arg, { req, run }) => {
+          const table = await this.get_table();
+          await table.insertRow({
+            run_id: run.id,
+            user_id: req.user?.id,
+            written_at: new Date(),
+            agent_trigger_id: run.trigger_id,
+            memory_type: "Episodic",
+            contents: arg.contents,
+          });
+          return "Recorded";
+        },
+      },
+      {
+        type: "function",
         process: async (arg, { req }) => {
+          const table = await this.get_table();
+
           const scState = getState();
           const language = scState.pg_ts_config;
           const use_websearch = scState.getConfig(
@@ -112,13 +137,7 @@ class LongTermMemory {
           );
           let rows = [];
           const phrases =
-            arg.query?.phrases ||
-            arg.phrases ||
-            (arg.phrase
-              ? [arg.phrase]
-              : arg.query?.phrase
-                ? [arg.query?.phrase]
-                : []);
+            typeof arg.phrases === "string" ? [arg.phrases] : arg.phrases;
           for (const phrase of phrases) {
             const my_rows = await table.getRows({
               _fts: {
@@ -130,91 +149,35 @@ class LongTermMemory {
                 schema: db.isSQLite ? undefined : db.getTenantSchema(),
               },
             });
-            if (this.hidden_fields) {
-              const hidden_fields = this.hidden_fields
-                .split(",")
-                .map((s) => s.trim());
-              my_rows.forEach((r) => {
-                hidden_fields.forEach((k) => {
-                  delete r[k];
-                });
-              });
-            }
             rows.push(...my_rows);
           }
           const pk = table.pk_name;
           rows = nubBy((r) => r[pk], rows);
-
+          //TODO sort most recent, only N memories
           if (rows.length)
-            if (!this.doc_format) return { rows };
-            else {
-              const responseText = rows
-                .map((row) => interpolate(this.doc_format, row, req.user))
-                .join("\n");
-              return { rows, responseText };
-            }
+            return (
+              "These memories were retrieved:\n\n" +
+              rows.map((r) => r.contents).join("\n")
+            );
           else
-            return {
-              responseText:
-                "There are no rows related to: " + phrases.join(" or "),
-            };
-        },
-        /*renderToolCall({ phrase }, { req }) {
-        return div({ class: "border border-primary p-2 m-2" }, phrase);
-      },*/
-        renderToolResponse: async ({ responseText, rows }, { req }) => {
-          if (rows) {
-            const view = View.findOne({ name: this.list_view });
-
-            if (view) {
-              const viewRes = await view.run(
-                { [table.pk_name]: { in: rows.map((r) => r[table.pk_name]) } },
-                { req },
-              );
-              return viewRes;
-            } else return "";
-          }
-          return div({ class: "border border-success p-2 m-2" }, responseText);
+            return "There are no memories related to: " + phrases.join(" or ");
         },
         function: {
-          name: this.toolName,
-          description: `Search the ${this.table_name} database table${
-            table.description ? ` (${table.description})` : ""
-          } by one or several search phrase or multiple search phrases matched against all fields in the table with full text search. The retrieved rows will be returned. If you want to search for documents matching any of several phrases, call this tool once with the phrases argument and give all the phrases you want to search for in one tool call.`,
+          name: "memory_retrieval",
+          description: `Search the memory bank by a search phrase`,
           parameters: {
             type: "object",
-            required: ["query"],
+            required: ["phrases"],
+            description:
+              "Search the memory bank by any of a number of phrases. This will return any memories that matches one or the other of the phrases",
             properties: {
-              query: {
-                anyOf: [
-                  {
-                    type: "object",
-                    required: ["phrase"],
-                    properties: {
-                      phrase: {
-                        type: "string",
-                        description:
-                          "The phrase to search the table with. The search phrase is the synatx used by web search engines: use double quotes for exact match, unquoted text for words in any order, dash (minus sign) to exclude a word. Do not use SQL or any other formal query language.",
-                      },
-                    },
-                  },
-                  {
-                    type: "object",
-                    required: ["phrases"],
-                    description:
-                      "Search the table by any of a number of phrases. This will return any document that matches one or the other of the phrases",
-                    properties: {
-                      phrases: {
-                        type: "array",
-                        description:
-                          "A phrase to search the table with. The search phrase is the synatx used by web search engines: use double quotes for exact match, unquoted text for words in any order, dash (minus sign) to exclude a word. Do not use SQL or any other formal query language.",
-                        items: {
-                          type: "string",
-                        },
-                      },
-                    },
-                  },
-                ],
+              phrases: {
+                type: "array",
+                description:
+                  "A phrase to search the memory bank with. The search phrase is the synatx used by web search engines: use double quotes for exact match, unquoted text for words in any order, dash (minus sign) to exclude a word. Do not use SQL or any other formal query language.",
+                items: {
+                  type: "string",
+                },
               },
             },
           },
